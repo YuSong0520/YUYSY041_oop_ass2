4a12c8968d206fd7b0a30dea5177b7fe6ef95244
YuSong0520 <yusong19940520@gmail.com>
2024-06-13 12:46:01 +1000

    Create properties for hot_tea and calculate price functionality

---
+++ hot_tea.py
@@ -0,0 +1,21 @@
+"""
+File: hot_tea.py
+Description: Defines the Topping class.
+Author: Yu Song
+ID: 110440805
+Username: YUYSY041
+This is my own work as defined by the University's Academic Misconduct Policy.
+"""
+from Bubble_tea import Bubble_tea
+
+
+class Hot_tea(Bubble_tea):
+    # create attributes common
+    def calculate_price(self):
+        size_cost = 0.85 if self.size == 'Medium' else 1.2 if self.size == 'Large' else 0
+        tea_type_cost = 0.24
+        toppings_cost = sum([topping.price for topping in self.toppings])
+        return Bubble_tea.base_price + size_cost + tea_type_cost + toppings_cost
+
+    def __str__(self):
+        return super().__str__()
